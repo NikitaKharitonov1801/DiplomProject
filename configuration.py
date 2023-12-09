e480580950f8bdf3aa91b383a92ecf23368255b8
NikitaKharitonov1801 <153395439+NikitaKharitonov1801@users.noreply.github.com>
2023-12-10 00:42:42 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "  https://abfe7e0d-72e8-42ba-a774-799fee283c13.serverhub.praktikum-services.ru/"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Nikita",
+    "lastName": "Kharitonov",
+    "address": "Bogorodskogo street apt. 5",
+    "metroStation": 4,
+    "phone": "+7 777 777 77 77",
+    "rentTime": 5,
+    "deliveryDate": "2020-05-07",
+    "comment": "No comments",
+    "color": [
+        "BLACK"
+    ]
+}
